77365f2a253c4eb3a1b41156ab8deddfc001ead8
Tiagomunerato <tiagomunerato@gmail.com>
2019-10-05 11:52:54 -0300

    Add project files.

---
+++ Formulario/Controllers/LogController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Formulario.Controllers
+{
+    public class LogController : Controller
+    {
+        // GET: Log
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Loga ()
+        {
+            string username = Request["username"];
+            string login = Request["login"];
+
+            if((login == "abc")&&(username=="tiago"))
+            {
+                
+                ViewBag.log = "LOGADO!!!!";
+
+            }
+            else
+            {
+                ViewBag.log = "SENHA INCORETA!!!";
+            }
+            return View();
+        }
+    }
+}
